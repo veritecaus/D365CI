e5212e231930dc2f8690c221891ca21f97db369c
Trevor Stephenson <Trevor.Stephenson@veritec.com.au>
2019-05-23 22:36:55 +0000

    Merged PR 1692: added autonumber seed
    added autonumber seed
    
    Related work items: #53619

---
+++ src/Veritec.Dynamics.CI.Common/Autonumber.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+
+namespace Veritec.Dynamics.CI.Common
+{
+    public class Autonumber : CiBase
+    {
+        public Autonumber(CrmParameter crmParameter) : base(crmParameter)
+        {
+
+        }
+
+        public void SetSeed(string entityName, string attributeName, long value, bool force = false)
+        {
+            SetAutoNumberSeedRequest req = new SetAutoNumberSeedRequest
+            {
+                EntityName = entityName,
+                AttributeName = attributeName,
+                Value = value
+            };
+
+            OrganizationService.Execute(req);
+        }
+
+        /// <summary>
+        /// Check if there is data in a target entity
+        /// </summary>
+        /// <param name="entityName"></param>
+        /// <returns>returns count of records that exist</returns>
+        public int TargetEntityRowCount(string entityName, string attributeName)
+        {
+            // using fetch as it's the only query type that supports aggregate functions.
+            var fetchXMLCount = $@"<fetch aggregate='true'>
+                                     <entity name = '{entityName}'>
+                                       <attribute name = '{attributeName}' alias='RecordCount' aggregate='count'/>
+                                     </entity >
+                                   </fetch>";
+
+            var fetchResult = OrganizationService.RetrieveMultiple(new FetchExpression(fetchXMLCount));
+            if (fetchResult.Entities.Count < 1) return 0;
+            var recordCount = (int)((Microsoft.Xrm.Sdk.AliasedValue)fetchResult[0]["RecordCount"]).Value;
+            return recordCount;
+
+        }
+    }
+}
---
+++ src/Veritec.Dynamics.CI.PowerShell/SetAutoNumberSeed.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Management.Automation;
+using Veritec.Dynamics.CI.Common;
+
+namespace Veritec.Dynamics.CI.PowerShell
+{
+    [Cmdlet("Set", "AutoNumberSeed")]
+    [OutputType(typeof(Boolean))]
+    public class SetAutoNumberSeed : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public string ConnectionString { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public int ConnectionTimeOutMinutes { get; set; } = 10;
+
+        [Parameter(Mandatory = true)]
+        public string EntityName { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public string AttributeName { get; set; }
+
+        [Parameter(Mandatory = true)]
+        public long Value { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public bool Force { get; set; } = false;
+
+        protected override void ProcessRecord()
+        {
+            var crmParameter = new CrmParameter(ConnectionString)
+            {
+                ConnectionTimeOutMinutes = ConnectionTimeOutMinutes
+            };
+
+            WriteObject($"Connecting ({crmParameter.GetConnectionStringObfuscated()}{Environment.NewLine})");
+
+            var autoNumber = new Autonumber(crmParameter);
+            try
+            {
+                var recordCount = autoNumber.TargetEntityRowCount(EntityName, AttributeName);
+                if (Force == true || recordCount == 0)
+                {
+                    /* only do this if the user forces it or there are no records in the target */
+                    autoNumber.SetSeed(EntityName, AttributeName, Value, Force);
+                    WriteObject($"Autonumber has been set to {Value}");
+                }
+                else
+                {
+                    WriteObject($"WARN - operation abandoned as the target entity contains at least {recordCount} records. Consider using the Force parameter.");
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                WriteError(new ErrorRecord(ex, "Error setting autonumber", ErrorCategory.InvalidOperation, AttributeName));
+            }
+        }
+    }
+}
